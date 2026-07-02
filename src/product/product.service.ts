12ceb44b4b178ab3192ee467898806fe6713de7b
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-03 00:46:24 +0600

    feat: implement product module with controller and service, add CreateProductDto

---
+++ src/product/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+
+@Injectable()
+export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createProduct(createProductDto: CreateProductDto) {
+    const { name, price, stock } = createProductDto;
+
+    return this.prisma.product.create({
+      data: {
+        name,
+        price: new Prisma.Decimal(price),
+        stock,
+      },
+    });
+  }
+}
