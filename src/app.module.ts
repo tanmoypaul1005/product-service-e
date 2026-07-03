9e0cef0b1fc11e62e9910c09d77800dca0a9d110
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-04 01:00:38 +0600

    cart module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, CartModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
