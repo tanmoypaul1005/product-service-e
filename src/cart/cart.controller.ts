9dad7b5b16e6a7f33da32f0144b8638aa72d7b25
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-04 01:16:28 +0600

    done massaggeblocker

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { CartService } from './cart.service';
 
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @MessagePattern('cart.addToCart')
+  async addToCart(@Payload() addToCartDto: AddToCartDto, @Ctx() context: RmqContext) {
+    const cartItem = await this.cartService.addToCart(addToCartDto);
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    channel.ack(originalMessage);
+
+    return cartItem;
+  }
+
+}
