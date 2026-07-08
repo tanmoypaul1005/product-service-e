ad7684251463bb98448187cd5f6f75a6b1b2c1f7
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2026-07-08 21:23:44 +0600

    brand section done

--- src/brand/brand.controller.ts
+++ src/brand/brand.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { BrandService } from './brand.service';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
+import { CreateBrandDto } from './dto/create.brand.dto';
 
 @Controller('brand')
-export class BrandController {}
+export class BrandController {
+    constructor(private readonly brandService: BrandService) { }
+
+    @MessagePattern('brand.createBrand')
+    async createBrand(@Body() data: CreateBrandDto, @Ctx() context: RmqContext) {
+        const brand = await this.brandService.createBrand(data);
+
+        const channel = context.getChannelRef();
+        const originalMessage = context.getMessage();
+        channel.ack(originalMessage);
+        return brand;
+    }
+}
